435f7f82515a56852bd87010f832f2d8d361481c
Ketan Prashant Meshram <ketanmeshram2310@gmail.com>
2026-03-25 18:28:58 +0530

    fix port binding for render

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.get('/api/health', (req, res) => {
 // VERY IMPORTANT
 const PORT = process.env.PORT || 10000;
 
-// Bind correctly
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
